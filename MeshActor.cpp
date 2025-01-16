ea84a6250705653945d93af1d93ba489b951a5e4
Lucas Bruel <81522727+Lurius-Kitsune@users.noreply.github.com>
2025-01-16 16:40:41 +0100

    - Patch Issue

--- MeshActor.cpp
+++ MeshActor.cpp
@@ -7,13 +7,13 @@ MeshActor::MeshActor(const float _radius, const size_t& _pointCount,
 	mesh = new MeshComponent(this, _radius, _path, _pointCount, _rect);
 	AddComponent(mesh);
 	renderMeshToken = M_GAME.BindOnRenderWindow(bind(&MeshActor::RenderMesh, this, placeholders::_1));
-	cout << renderMeshToken << endl;
 }
 
 MeshActor::MeshActor(const Vector2f& _size, const string& _path, const IntRect& _rect)
 {
 	mesh = new MeshComponent(this, _size, _path, _rect);
 	AddComponent(mesh);
+	renderMeshToken = M_GAME.BindOnRenderWindow(bind(&MeshActor::RenderMesh, this, placeholders::_1));
 }
 
 MeshActor::~MeshActor()
